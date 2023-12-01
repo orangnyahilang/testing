40eff669fbdd8962e8b822b0ed1b440076131fdb
orangnyahilang <aryasatyaibnusani@gmail.com>
2023-12-01 14:27:06 +0700

    commit ketiga

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
-import { Routes, Route, BrowserRouter } from "react-router-dom"
-
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Application from './Application';
-
 
 function App() {
   return (
     <div className="App">
-       <BrowserRouter>
-       <Routes>
-         <Route>
-
-           <Route path="/testing" element={<Application/>} />
-
-         </Route>
-       </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/testing" element={<Application />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
